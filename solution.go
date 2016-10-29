676634807ed430c63324208a3719b27663c1186b
maiq <subtlepseudonym@gmail.com>
2016-10-29 02:16:21 -0400

    index update, prints las_address

--- solution.go
+++ solution.go
@@ -8,20 +8,39 @@ import (
 	"net/http"
 	"log"
 	"os"
-	"fmt"
+	"os/exec"
+	"bytes"
+	"strings"
+)
+
+var (
+	las_port string = ":8080"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the index.")
+	http.ServeFile(w, r, "web/index.html")
 }
 
 func init() {
 	// Enable logging to local file
 	log.SetOutput(os.Stdout)
+
+	// Print public ip address and ListenAndServe port
+	cmd := exec.Command("wget", `http://ipinfo.io/ip`, "-qO", "-")
+	std_stream, err := cmd.StdoutPipe()
+	if err != nil { log.Fatal(err) }
+	if err := cmd.Start(); err != nil { 
+		log.Println("Error retrieving public ip - you may not be connected to the internet")
+		log.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(std_stream)
+	pub_ip := strings.TrimSpace(buf.String())
+	log.Println("ListenAndServe at", pub_ip + las_port)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(las_port, nil))
 }
